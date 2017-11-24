msaelices/react-onsenui-redux-weather

commit f3b2de5b46a16b739de619d9b62947fb56eeef1d

feat (pull-to-refresh): Pull-to-refresh feature in the home screen.

src/components/MainPage.js
@@ -1,18 +1,89 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 import {
-  Page
+  Icon,
+  Page,
+  PullHook
 } from 'react-onsenui';
 
 import NavBar from './NavBar';
 import LocationList from '../containers/LocationList';
 import AddLocation from '../containers/AddLocation';
+import {updateForecasts} from '../actions';
 
-const MainPage = ({navigator}) => (
-  <Page renderToolbar={() => <NavBar title='Onsen Weather' navigator={navigator} />}>
-    <LocationList navigator={navigator} />
-    <AddLocation />
-  </Page>
-);
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    this.state = {
+      pullingState: 'initial',
+    };
+    this.dispatch = props.dispatch;
+
+    // bind
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
+    this.getContent = this.getContent.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({pullingState: e.state});
+  }
+
+  handleLoad(done) {
+    // refresh all the forecasts
+    this.dispatch(updateForecasts());
+    // simulate a 1 second timeout to hide the pull hook
+    // this should be wait for all the forecasts
+    setTimeout(() => done(), 1000);
+  }
+
+  getContent() {
+    switch (this.state.pullingState) {
+      case 'initial':
+        return (
+          <span>
+            <Icon size={20} spin={false} icon='long-arrow-down' class="left" />
+            Pull to refresh
+          </span>
+        );
+      case 'preaction':
+        return (
+          <span>
+            <Icon size={20} spin={false} icon='long-arrow-up' class="left" />
+            Release
+          </span>
+        );
+        return 'Release';
+      case 'action':
+        return (
+          <span>
+            <Icon size={20} spin={true} icon='refresh' class="left" />
+            Loading...
+          </span>
+        );
+    };
+  }
+
+  render() {
+    const { navigator } = this;
+    return (
+      <Page renderToolbar={() => <NavBar title='Onsen Weather' navigator={navigator} />}>
+        <PullHook
+          onChange={this.handleChange}
+          onLoad={this.handleLoad}
+        >
+          {this.getContent()}
+        </PullHook>
+        <LocationList navigator={navigator} />
+        <AddLocation />
+      </Page>
+    );
+  }
+}
+MainPage = connect()(MainPage)
 
 export default MainPage;

src/actions/index.js
@@ -45,6 +45,16 @@ export const setFetchError = id => ({
   id
 });
 
+export const updateForecasts = (id) => {
+
+  return (dispatch, getState) => {
+    Object.entries(getState().locations).map(([id, location]) => {
+      console.log('fetching...' + id);
+      dispatch(fetchWeather(id));
+    });
+  };
+};
+
 export const fetchWeather = (id) => {
   /*
    * This function requests and receives the

src/onsen-css-components.css
@@ -3,3 +3,9 @@
 @import url('../node_modules/onsenui/css/font_awesome/css/font-awesome.min.css');
 @import url('../node_modules/onsenui/css/material-design-iconic-font/css/material-design-iconic-font.min.css');
 @import url('../node_modules/onsenui/css-components-src/src/iphonex-support/index.css');
+
+.ons-icon {
+  &.left {
+    margin-right: 5px;
+  }
+}

commit 9981ebff933bbc280ff815455178fa47903d7d44

Fix a bug caused after f3b2de5b46a16b739de619d9b62947fb56eeef1d which makes location weather page breaks.

src/components/MainPage.js
@@ -66,7 +66,7 @@ class MainPage extends React.Component {
   }
 
   render() {
-    const { navigator } = this;
+    const { navigator } = this.props;
     return (
       <Page renderToolbar={() => <NavBar title='Onsen Weather' navigator={navigator} />}>
         <PullHook